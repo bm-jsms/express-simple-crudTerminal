813b3f8e5ebf73d41ca8813efa2899fc35a2c56d
Josue Emaxs <josueemaxsalazar04@gmail.com>
2023-11-11 12:50:53 -0500

    setup

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ import Express from 'express';
 import morgan from 'morgan';
 const app = Express();
 const port = 3000;
+const products = [
+	{
+		id: 1,
+		name: 'Product 1',
+		price: 100,
+	},
+];
 
 app.use(morgan('dev'));
+app.use(Express.json());
 
 app.get('/products', (req, res) => {
-	res.send('Get products');
+	res.json(products);
 });
 
 app.post('/products', (req, res) => {
-	res.send('Post products');
+	const newProduct = {
+		...req.body,
+		id: products.length + 1,
+	};
+	products.push(newProduct);
+	res.send(newProduct);
 });
 
 app.put('/products', (req, res) => {
@@ -22,7 +35,13 @@ app.delete('/products', (req, res) => {
 });
 
 app.get('/products/:id', (req, res) => {
-	res.send(`Get product with id: ${req.params.id}`);
+	console.log(req.params.id);
+	const profuctFound = products.find(
+		product => product.id === parseInt(req.params.id),
+	);
+	if (!profuctFound) return res.status(404).send('Product not found');
+	console.log(profuctFound);
+	res.json(profuctFound);
 });
 
 app.listen(port, () => {
